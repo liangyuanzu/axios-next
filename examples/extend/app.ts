769a8074ff96047ccfa3a76ebe89d76d3f9f383e
liangyuanzu <liangyuanzu@gmail.com>
2021-06-05 17:59:32 +0800

    feat: 扩展接口 demo

---
+++ examples/extend/app.ts
@@ -0,0 +1,31 @@
+import axios from '../../src/index'
+
+axios({
+  url: '/extend/post',
+  method: 'post',
+  data: {
+    msg: 'hi'
+  }
+})
+
+axios.request({
+  url: '/extend/post',
+  method: 'post',
+  data: {
+    msg: 'hello'
+  }
+})
+
+axios.get('/extend/get')
+
+axios.post('/extend/post', { msg: 'post' })
+
+axios.put('/extend/put', { msg: 'put' })
+
+axios.patch('/extend/patch', { msg: 'patch' })
+
+axios.delete('/extend/delete')
+
+axios.head('/extend/head')
+
+axios.options('/extend/options')
