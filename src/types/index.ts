48964934a3cf8bfdf2f947857f4f4b4322aa20ac
liangyuanzu <liangyuanzu@gmail.com>
2021-06-06 23:38:06 +0800

    feat: 拦截器管理类

--- src/types/index.ts
+++ src/types/index.ts
@@ -66,3 +66,17 @@ export interface AxiosError extends Error {
   request?: any
   response?: AxiosResponse
 }
+
+export interface AxiosInterceptorManager<T> {
+  use(resolved: ResolvedFn<T>, rejected?: RejectedFn): number
+
+  eject(id: number): void
+}
+
+export interface ResolvedFn<T = any> {
+  (val: T): T | Promise<T>
+}
+
+export interface RejectedFn {
+  (error: any): any
+}
