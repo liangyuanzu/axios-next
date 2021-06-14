5b4edc53efa31054389186789399cbd327eb3a94
liangyuanzu <liangyuanzu@gmail.com>
2021-06-14 20:49:50 +0800

    test: headers

--- src/helpers/headers.ts
+++ src/helpers/headers.ts
@@ -30,10 +30,10 @@ export function parseHeaders(headers: string): object {
   if (!headers) return parsed
 
   headers.split('\r\n').forEach(line => {
-    let [key, val] = line.split(':')
+    let [key, ...vals] = line.split(':')
     key = key.trim().toLowerCase()
     if (!key) return
-    if (val) val = val.trim()
+    const val = vals.join(':').trim()
     parsed[key] = val
   })
   return parsed
